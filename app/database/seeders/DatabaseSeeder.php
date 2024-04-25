9b1c750c033bd7a801b09b60c671c525f743dc36
imrane sarsri <imransarsi.ii@gmail.com>
2024-04-25 16:27:55 +0100

    add parameter seeder

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -2,29 +2,13 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\GestionParametres\GradeSeeder;
-use Database\Seeders\GestionParametres\MotifSeeder;
-use Database\Seeders\GestionParametres\VilleSeeder;
-use Database\Seeders\GestionParametres\FonctionSeeder;
-use Database\Seeders\GestionParametres\AvancementSeeder;
-use Database\Seeders\GestionParametres\SpecialiteSeeder;
-use Database\Seeders\GestionParametres\EtablissementSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call(AvancementSeeder::class);
-        $this->call(EtablissementSeeder::class);
-        $this->call(FonctionSeeder::class);
-        $this->call(GradeSeeder::class);
-        $this->call(MotifSeeder::class);
-        $this->call(SpecialiteSeeder::class);
-        $this->call(VilleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ParametresSeeder::class);
     }
 }
