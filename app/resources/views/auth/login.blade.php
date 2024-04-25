283b8bbb6ea7c14fe6cf920cdf8ea76ae42eea0b
jalil betroji <jalil.betroji@gmail.com>
2024-04-25 09:27:16 +0100

    layouts and auth added

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -1,23 +1,25 @@
 <x-laravel-ui-adminlte::adminlte-layout>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <body class="hold-transition login-page">
         <div class="login-box">
             <div class="login-logo">
-                <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
+                <img src="{{ asset('images/logo.png') }}" alt="" srcset="" width="90px">
+                <h4>Gestion personnels</h4>
             </div>
             <!-- /.login-logo -->
 
             <!-- /.login-box-body -->
             <div class="card">
                 <div class="card-body login-card-body">
-                    <p class="login-box-msg">Sign in to start your session</p>
+                    <p class="login-box-msg">Connectez-vous pour commencer votre session</p>
 
                     <form method="post" action="{{ url('/login') }}">
                         @csrf
 
                         <div class="input-group mb-3">
-                            <input type="email" name="email" value="{{ old('email') }}" placeholder="Email"
-                                class="form-control @error('email') is-invalid @enderror">
+                            <input type="email" name="email" value="{{ old('email') }}" placeholder="E-mail"
+                                class="form-control @error('email') is-invalid @enderror" autofocus>
                             <div class="input-group-append">
                                 <div class="input-group-text"><span class="fas fa-envelope"></span></div>
                             </div>
@@ -27,7 +29,7 @@
                         </div>
 
                         <div class="input-group mb-3">
-                            <input type="password" name="password" placeholder="Password"
+                            <input type="password" name="password" placeholder="Mot de passe"
                                 class="form-control @error('password') is-invalid @enderror">
                             <div class="input-group-append">
                                 <div class="input-group-text">
@@ -41,26 +43,19 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-8">
+                            <div class="col-7">
                                 <div class="icheck-primary">
                                     <input type="checkbox" id="remember">
-                                    <label for="remember">Remember Me</label>
+                                    <label for="remember">Se souvenir de moi</label>
                                 </div>
                             </div>
 
-                            <div class="col-4">
-                                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                            <div class="col-5">
+                                <button type="submit" class="btn btn-info btn-block">Se connecter</button>
                             </div>
 
                         </div>
                     </form>
-
-                    <p class="mb-1">
-                        <a href="{{ route('password.request') }}">I forgot my password</a>
-                    </p>
-                    <p class="mb-0">
-                        <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
-                    </p>
                 </div>
                 <!-- /.login-card-body -->
             </div>
