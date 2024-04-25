023fbf385a4848f1098db10820e630097a0b8fe9
jalil betroji <jalil.betroji@gmail.com>
2024-04-25 10:08:17 +0100

    home route updated

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/');
+    return view('home');
 })->middleware('auth')->name('home');
 
 Auth::routes(['register' => true]);
