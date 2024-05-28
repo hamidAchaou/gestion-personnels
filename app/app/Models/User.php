d7108a494982a7baeb33a458e6555bead457b707
amine.lamchatab <alamchatab@gmail.com>
2024-05-28 16:54:40 +0100

    save

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -2,48 +2,97 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+// use spatie package
 use Spatie\Permission\Traits\HasRoles;
+// use parametres
+use App\Models\pkg_Parametres\Ville;
+use App\Models\pkg_Parametres\Fonction;
+use App\Models\pkg_Parametres\Avancement;
+use App\Models\pkg_Parametres\Specialite;
+use App\Models\pkg_Parametres\Etablissement;
+// use models of packages
+use App\Models\pkg_OrderDesMission\Mission;
+use App\Models\pkg_Absences\Absence;
+use App\Models\pkg_Conges\Conge;
+
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    public const ADMIN = "admin";
-    public const APPRENANT = "apprenant";
-    public const FORMATEUR = "formateur";
-
+    const ADMIN = "admin";
+    const RESPONSABLE = "responsable";
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
+        'nom_arab',
+        'prenom_arab',
+        'cin',
+        'date_naissance',
+        'telephone',
         'email',
         'password',
+        'address',
+        'images',
+        'matricule',
+        'ville_id',
+        'fonction_id',
+        'ETPAffectation_id',
+        'grade_id',
+        'specialite_id',
+        'etablissement_id',
+        'avancement_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function ville()
+    {
+        return $this->belongsTo(Ville::class, 'ville_id');
+    }
+    public function fonction()
+    {
+        return $this->belongsTo(Fonction::class, 'fonction_id');
+    }
+    public function specialite()
+    {
+        return $this->belongsTo(Specialite::class, 'specialite_id');
+    }
+    public function etablissement()
+    {
+        return $this->belongsTo(Etablissement::class, 'etablissement_id');
+    }
+    public function etp_Affectation()
+    {
+        return $this->belongsTo(Etablissement::class, 'ETPAffectation_id');
+    }
+    public function avancement()
+    {
+        return $this->belongsTo(Avancement::class, 'avancement_id');
+    }
+    public function absences()
+    {
+        return $this->hasMany(Absence::class, 'user_id');
+    }
+
+    public function conges()
+    {
+        return $this->belongsToMany(Conge::class, 'personnels_conges', 'user_id', 'conges_id')->withTimestamps();
+    }
+
+    public function missions()
+    {
+        return $this->belongsToMany(Mission::class, 'mission_personnel', 'user_id', 'mission_id')->withTimestamps();
+    }
+
 }
---
+++ app/database/seeders/AutorisationsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Database\Seeders\pkg_autorisations\{
+    RoleSeeder,
+};
+
+
+class AutorisationsSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $this->call(AutorisationsSeeder::Classes());
+    }
+
+    public static function Classes(): array
+    {
+        return [
+            RoleSeeder::class,
+        ];
+    }
+}
