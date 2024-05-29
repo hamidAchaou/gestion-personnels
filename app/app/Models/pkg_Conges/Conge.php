28ead2b1d68ed96e7b6239056ac0f04b9a77e077
Hamid <hamidachaou379@gmail.com>
2024-05-29 20:41:36 +0100

    add seeder

--- app/app/Models/pkg_Conges/Conge.php
+++ app/app/Models/pkg_Conges/Conge.php
@@ -4,6 +4,7 @@ namespace App\Models\pkg_Conges;
 
 use App\Models\pkg_Parametres\Motif;
 use App\Models\pkg_PriseDeServices\Personnel;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,7 +20,7 @@ class Conge extends Model
 
     public function users()
     {
-        return $this->belongsToMany(Personnel::class, 'personnels_conges', 'conges_id', 'user_id');
+        return $this->belongsToMany(User::class, 'personnels_conges', 'conges_id', 'user_id');
     }
 
     public function motifs()
