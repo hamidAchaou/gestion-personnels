1792112ab8aa1833f305303154eee69521f3a531
Hamid <hamidachaou379@gmail.com>
2024-06-05 14:32:46 +0100

    fix show

--- app/resources/views/pkg_Conges/conges/details-calcule.blade.php
+++ app/resources/views/pkg_Conges/conges/details-calcule.blade.php
@@ -1,17 +1,15 @@
 <div class="tab-pane fade" id="detailsJourReson" role="tabpanel" aria-labelledby="custom-tabs-one-moreDetails-tab">
-    <!-- anée 1 -->
+    <!-- Année 1 -->
     <div class="card collapsed-card">
         <div class="card-header border-0">
             <h3 class="card-title">
-                <i class="fa-solid fa-calendar-days"></i> 2023 (jours restan = 12)
+                <i class="fa-solid fa-calendar-days"></i> {{ $lastYear }} (jours restant = {{ $CongesLastYear->sum('jours_restants') }})
             </h3>
-
             <div class="card-tools">
                 <button type="button" class="btn btn-teal btn-sm" data-card-widget="collapse" title="Collapse">
                     <i class="fas fa-plus"></i>
                 </button>
             </div>
-
         </div>
         <div class="card-body" style="display: none;">
             <table class="table table-striped text-nowrap">
@@ -23,76 +21,41 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>2024-02-28</td>
-                        <td>2024-03-02</td>
-                        <td class="text-center">5</td>
-                    </tr>
-                    <tr>
-                        <td>2024-02-28</td>
-                        <td>2024-03-02</td>
-                        <td class="text-center">3</td>
-                    </tr>
-                    <tr>
-                        <td>2024-02-28</td>
-                        <td>2024-03-02</td>
-                        <td class="text-center">7</td>
-                    </tr>
-                    <tr>
-                        <td>2023-12-02</td>
-                        <td>2024-12-09</td>
-                        <td class="text-center">6</td>
-                    </tr>
-                    <tr>
-                        <td>2023-07-12</td>
-                        <td>2023-12-19</td>
-                        <td class="text-center">5</td>
-                    </tr>
+                    @foreach($CongesLastYear as $conge)
+                        <tr>
+                            <td>{{ $conge->date_debut }}</td>
+                            <td>{{ $conge->date_fin }}</td>
+                            <td class="text-center">{{ $conge->nombre_jours }}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td>total</td>
+                        <td>Total</td>
                         <td></td>
-                        <td class="text-center">= 26</td>
+                        <td class="text-center">{{ $CongesLastYear->sum('nombre_jours') }}</td>
                     </tr>
                     <tr>
-                        <td>jours restan</td>
+                        <td>Jours restants</td>
                         <td></td>
-                        <td class="text-center">22</td>
-                        <!-- <td class="text-center">(22 + 12) = 34 - 26 = 8</td> -->
-                    </tr>
-                    <tr>
-                        <td>jours restan 2022</td>
-                        <td></td>
-                        <td class="text-center">16</td>
-                        <!-- <td class="text-center">(22 + 12) = 34 - 26 = 8</td> -->
-                    </tr>
-                    <tr>
-                        <td>total jours restan</td>
-                        <td></td>
-                        <td class="text-center">(22 + 16) = 38 - 26 = 12</td>
+                        <td class="text-center">{{ $CongesLastYear->sum('jours_restants') }}</td>
                     </tr>
                 </tfoot>
             </table>
         </div>
-
-        <div class="card-footer bg-transparent" style="display: none;">
-
-        </div>
     </div>
-    <!-- anée 2 -->
+
+    <!-- Année 2 -->
     <div class="card">
         <div class="card-header border-0">
             <h3 class="card-title">
-                <i class="fa-solid fa-calendar-days"></i> 2024 (jours restan = 8)
+                <i class="fa-solid fa-calendar-days"></i> {{ $firstYear }} (jours restant = {{ $CongesFirstYear->sum('jours_restants') }})
             </h3>
-            <!-- <h6>jour restants 12</h6> -->
             <div class="card-tools">
                 <button type="button" class="btn btn-teal btn-sm" data-card-widget="collapse" title="Collapse">
                     <i class="fas fa-minus"></i>
                 </button>
             </div>
-
         </div>
         <div class="card-body" style="display: block;">
             <table class="table table-striped text-nowrap">
@@ -104,61 +67,27 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>2024-02-28</td>
-                        <td>2024-03-02</td>
-                        <td class="text-center">5</td>
-                    </tr>
-                    <tr>
-                        <td>2024-02-28</td>
-                        <td>2024-03-02</td>
-                        <td class="text-center">3</td>
-                    </tr>
-                    <tr>
-                        <td>2024-02-28</td>
-                        <td>2024-03-02</td>
-                        <td class="text-center">7</td>
-                    </tr>
-                    <tr>
-                        <td>2023-12-02</td>
-                        <td>2024-12-09</td>
-                        <td class="text-center">6</td>
-                    </tr>
-                    <tr>
-                        <td>2023-07-12</td>
-                        <td>2023-12-19</td>
-                        <td class="text-center">5</td>
-                    </tr>
+                    @foreach($CongesFirstYear as $conge)
+                        <tr>
+                            <td>{{ $conge->date_debut }}</td>
+                            <td>{{ $conge->date_fin }}</td>
+                            <td class="text-center">{{ $conge->nombre_jours }}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td>total</td>
+                        <td>Total</td>
                         <td></td>
-                        <td class="text-center">= 26</td>
+                        <td class="text-center">{{ $CongesFirstYear->sum('nombre_jours') }}</td>
                     </tr>
                     <tr>
-                        <td>jours restan</td>
+                        <td>Jours restants</td>
                         <td></td>
-                        <td class="text-center">22</td>
-                        <!-- <td class="text-center">(22 + 12) = 34 - 26 = 8</td> -->
-                    </tr>
-                    <tr>
-                        <td>jours restan 2023</td>
-                        <td></td>
-                        <td class="text-center">12</td>
-                        <!-- <td class="text-center">(22 + 12) = 34 - 26 = 8</td> -->
-                    </tr>
-                    <tr>
-                        <td>total jours restan</td>
-                        <td></td>
-                        <td class="text-center">(22 + 12) = 34 - 26 = 8</td>
+                        <td class="text-center">{{ $CongesFirstYear->sum('jours_restants') }}</td>
                     </tr>
                 </tfoot>
             </table>
-        </div>
-
-        <div class="card-footer bg-transparent" style="display: block;">
-
         </div>
     </div>
 </div>
