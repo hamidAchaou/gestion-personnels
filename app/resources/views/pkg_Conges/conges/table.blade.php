c9a41f4e8317f106a44b1b6b333923a6be501c71
Hamid <hamidachaou379@gmail.com>
2024-06-07 23:03:30 +0100

    fix disagne footer table page index

--- app/resources/views/pkg_Conges/conges/table.blade.php
+++ app/resources/views/pkg_Conges/conges/table.blade.php
@@ -20,12 +20,14 @@
                     <td class="text-center">{{ $conge->date_fin }}</td>
                     <td class="text-center">{{ $conge->joursRestants }}</td>
                     <td class="text-center">
-                        <a href="{{ route('conges.decision', ['conge' => $personnel->id]) }}" class='btn btn-default btn-sm'>
+                        <a href="{{ route('conges.decision', ['conge' => $personnel->id]) }}"
+                            class='btn btn-default btn-sm'>
                             <i class="fa-regular fa-file"></i>
                         </a>
                     </td>
                     <td class="text-center">
-                        <a href="{{ route('conges.show', ['conge' => $personnel->id]) }}" class='btn btn-default btn-sm'>
+                        <a href="{{ route('conges.show', ['conge' => $personnel->id]) }}"
+                            class='btn btn-default btn-sm'>
                             <i class="far fa-eye"></i>
                         </a>
                     </td>
@@ -40,26 +42,33 @@
     <tfoot>
         <tr>
             <td colspan="7">
-                <div class="d-flex justify-content-between align-items-center p-2 card-footer" id="card-footer">
-                    <div class="d-flex align-items-center mb-2 ml-2 mt-2">
-                        <form action="{{ route('conges.import') }}" method="post" class="mt-2" enctype="multipart/form-data" id="importForm">
-                            @csrf
-                            <label for="upload" class="btn btn-default btn-sm">
-                                <i class="fa-solid fa-file-arrow-down"></i>
-                                IMPORTER
-                            </label>
-                            <input type="file" id="upload" name="file" style="display:none;" onchange="submitForm()" />
-                        </form>
-                
-                        <button type="button" class="btn btn-default btn-sm mt-0 mx-2 font-weight-bold" data-bs-toggle="modal" data-bs-target="#filterModal">
-                            <i class="fas fa-file-export"></i>
-                            EXPORTER
-                        </button>
-                    </div>
-                    <div>
-                        <ul class="pagination m-0 float-right">
-                            {{ $conges->links() }}
-                        </ul>
+                <div class="card-footer" id="card-footer">
+                    <div class="w-100 d-flex justify-content-between align-items-center">
+                        <div class="">
+                            <div class="d-flex align-items-center">
+                                <form action="{{ route('conges.import') }}" method="post" class="mt-2"
+                                    enctype="multipart/form-data" id="importForm">
+                                    @csrf
+                                    <label for="upload" class="btn btn-default btn-sm">
+                                        <i class="fa-solid fa-file-arrow-down"></i>
+                                        IMPORTER
+                                    </label>
+                                    <input type="file" id="upload" name="file" style="display:none;"
+                                        onchange="submitForm()" />
+                                </form>
+
+                                <button type="button" class="btn btn-default btn-sm mt-0 mx-2 font-weight-bold"
+                                    data-bs-toggle="modal" data-bs-target="#filterModal">
+                                    <i class="fas fa-file-export"></i>
+                                    EXPORTER
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            <ul class="pagination m-0 float-right">
+                                {{ $conges->links() }}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </td>
