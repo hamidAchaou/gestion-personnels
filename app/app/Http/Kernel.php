ab0521d19eacb3554fca2ed13fa93f14736140d7
Hamid <hamidachaou379@gmail.com>
2024-06-09 00:52:58 +0100

    fix authorisation Etablissement

--- app/app/Http/Kernel.php
+++ app/app/Http/Kernel.php
@@ -65,4 +65,10 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        // Other middleware entries...
+        'authorize.etablissement' => \App\Http\Middleware\AuthorizeEtablissement::class,
+    ];
+    
 }
