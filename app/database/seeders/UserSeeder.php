abeead88db5266ba696643351d3fe6cd0cc499d1
jalil betroji <jalil.betroji@gmail.com>
2024-06-08 13:58:07 +0100

    save

--- app/database/seeders/UserSeeder.php
+++ app/database/seeders/UserSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
 
@@ -54,15 +54,53 @@ class UserSeeder extends Seeder
 
         fclose($csvFile);
 
-        $adminUser = User::where("email", "admin@solicode.co")->first();
-        if ($adminUser) {
-            $adminUser->assignRole(User::ADMIN);
-        }
+        // ==========================================================
+        // =========== Add Seeder Permission Assign Role ============
+        // ==========================================================
+        $adminRole = User::ADMIN;
+        $responsableRole = User::RESPONSABLE;
+        $roleAdmin = Role::where('name', $adminRole)->first();
+        $roleResponsable = Role::where('name', $responsableRole)->first();
 
-        $responsableUser = User::where("email", "responsable@solicode.co")->first();
-        if ($responsableUser) {
-            $responsableUser->assignRole(User::RESPONSABLE);
-        }
+        Schema::disableForeignKeyConstraints();
+        Schema::enableForeignKeyConstraints();
 
+        $csvFile = fopen(base_path("database/data/pkg_PriseDeServices/personnels/PersonnelsPermissions.csv"), "r");
+        $firstline = true;
+        while (($data = fgetcsv($csvFile)) !== FALSE) {
+            if (!$firstline) {
+                Permission::create([
+                    "name" => $data['0'],
+                    "guard_name" => $data['1'],
+                ]);
+
+                if ($roleAdmin) {
+                    // If the role exists, update its permissions
+                    $roleAdmin->givePermissionTo($data['0']);
+                } else {
+                    // If the role doesn't exist, create it and give permissions
+                    $roleAdmin = Role::create([
+                        'name' => $adminRole,
+                        'guard_name' => 'web',
+                    ]);
+                    $roleAdmin->givePermissionTo($data['0']);
+                }
+                // Only give specific permissions to the 'responsable' role
+                if (in_array($data['0'], ['index-PersonnelController', 'show-PersonnelController', 'export-PersonnelController'])) {
+                    if ($roleResponsable) {
+                        $roleResponsable->givePermissionTo($data['0']);
+                    } else {
+                        $roleResponsable = Role::create([
+                            'name' => $responsableRole,
+                            'guard_name' => 'web',
+                        ]);
+                        $roleResponsable->givePermissionTo($data['0']);
+                    }
+                }
+
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 }
